8c448504b14a12c725a6b66a6be4a4259cdff01d
mihir123456789 <109252112+mihir123456789@users.noreply.github.com>
2022-09-13 15:55:05 -0400

    Updates

---
+++ precache-manifest.d55eefdbdd923fc48bd4f827eeb09a3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "860baaffeafe477327df",
+    "url": "/reactProject/static/css/main.f7c1e135.chunk.css"
+  },
+  {
+    "revision": "860baaffeafe477327df",
+    "url": "/reactProject/static/js/main.9b071350.chunk.js"
+  },
+  {
+    "revision": "8634270e03219b2f15e0",
+    "url": "/reactProject/static/js/runtime~main.76ed094f.js"
+  },
+  {
+    "revision": "d0421e72ff746efc9d9f",
+    "url": "/reactProject/static/css/2.fb1ac45d.chunk.css"
+  },
+  {
+    "revision": "d0421e72ff746efc9d9f",
+    "url": "/reactProject/static/js/2.0c45ff54.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/reactProject/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "65238819b66d371b5729f03d326307c3",
+    "url": "/reactProject/index.html"
+  }
+];
